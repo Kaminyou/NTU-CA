988351bebf4025a46c951949415320e9588995b8
kaminyou ho <kaminyouho@aetherai.com>
2023-11-24 02:41:13 +0800

    finish cpu

--- lab2/code/src/CPU.v
+++ lab2/code/src/CPU.v
@@ -7,59 +7,58 @@ module CPU
 input clk_i;
 input rst_i;
 
-wire [31:0] pc, pc_next, IF_PC_i, IF_IR;
+// Program Counter Signals
+wire [31:0] pc, pc_next, IF_pc, IF_instr;
+
+// Instruction Fetch (IF) Stage Signals
 wire NoOp, PCWrite, Stall;
+wire [31:0] ID_pc, ID_instr;
 
-wire [31:0] ID_IR;
-wire [31:0] ID_PC;
-
-wire [31:0] ID_data1, ID_data2, ID_imme;
-wire [4:0] ID_RD, ID_Rs1, ID_Rs2;
-assign ID_RD = ID_IR[11:7];
-assign ID_Rs1 = ID_IR[19:15];
-assign ID_Rs2 = ID_IR[24:20];
-
-wire ID_RegWrite, ID_MemtoReg;
-wire ID_MemRead, ID_MemWrite;
-wire ID_ALUSrc, ID_Branch;
+// Instruction Decode (ID) Stage Registers and Control Signals
+wire [31:0] ID_data1, ID_data2, ID_immed;
+wire [4:0] ID_Rd, ID_Rs1, ID_Rs2;
 wire [1:0] ID_ALUOp;
+wire ID_RegWrite, ID_MemtoReg, ID_MemRead, ID_MemWrite, ID_ALUSrc, ID_Branch;
+wire ID_branch_ctr, ID_FlushIF;
 
-wire ID_to_branch, ID_FlushIF;
-assign ID_to_branch = (ID_data1 == ID_data2) & ID_Branch;
-assign ID_FlushIF = ID_to_branch;
+// Instruction Decode (ID) Stage Assignments
+assign ID_Rd = ID_instr[11:7];
+assign ID_Rs1 = ID_instr[19:15];
+assign ID_Rs2 = ID_instr[24:20];
+assign ID_branch_ctr = (ID_data1 == ID_data2) & ID_Branch;
+assign ID_FlushIF = ID_branch_ctr;
 
-wire [31:0] ID_branch_PC;
-assign ID_branch_PC = (ID_imme << 1) + ID_PC;
+// Branch Calculation
+wire [31:0] ID_branch_pc;
+assign ID_branch_pc = (ID_immed << 1) + ID_pc;
 
-wire [11:0] imme_o;
+// Immediate Value Extraction
+wire [11:0] immed;
 
-wire [31:0] EX_A, EX_B, EX_imme, EX_PC;
-wire [4:0] EX_RD, EX_Rs1, EX_Rs2;
-wire [31:0] EX_IR;
-
-wire EX_RegWrite, EX_MemtoReg, EX_MemRead, EX_MemWrite, EX_ALUSrc;
+// Execution (EX) Stage Signals
+wire [31:0] EX_data1, EX_data2, EX_immed, EX_pc, EX_instr;
+wire [4:0] EX_Rd, EX_Rs1, EX_Rs2;
 wire [1:0] EX_ALUOp;
-
+wire EX_RegWrite, EX_MemtoReg, EX_MemRead, EX_MemWrite, EX_ALUSrc;
 wire signed [31:0] EX_ALUout;
 
-wire [31:0] MUX_A_o, MUX_B_o;
-wire [31:0] ALU_A_i, ALU_B_i;
+// ALU Control
 wire [2:0] ALUctl;
 
-assign ALU_A_i = MUX_A_o;
+// Multiplexer Outputs for ALU
+wire [31:0] MUX_First_o, MUX_Second_o, ALUdata2_i;
 
+// Memory Access (MEM) Stage Signals
+wire [31:0] MEM_ALUout, MEM_WriteData, MEM_MemoryData;
+wire [4:0] MEM_Rd;
+wire MEM_RegWrite, MEM_MemtoReg, MEM_MemRead, MEM_MemWrite;
 
-wire [31:0] MEM_ALUout, MEM_WD;
-wire [4:0] MEM_RD;
-wire MEM_RegWrite, MEM_MemtoReg, MEM_MemRead,MEM_MemWrite;
-
-wire [31:0] MEM_MD;
-
-wire [31:0] WB_ALUout, WB_MD;
+// Write Back (WB) Stage Signals
+wire [31:0] WB_ALUout, WB_MemoryData, WB_RDdata;
 wire [4:0] WB_Rd;
 wire WB_MemtoReg, WB_RegWrite;
 
-wire [31:0] WB_RDdata;
+// Forwarding Unit Signals
 wire [1:0] Forward_A, Forward_B;
 
 
@@ -67,7 +66,7 @@ Forwarder Forwarder(
 	.EX_Rs1(EX_Rs1),
 	.EX_Rs2(EX_Rs2),
 	.MEM_RegWrite(MEM_RegWrite),
-	.MEM_Rd(MEM_RD),
+	.MEM_Rd(MEM_Rd),
 	.WB_RegWrite(WB_RegWrite),
 	.WB_Rd(WB_Rd),
 	.Forward_A(Forward_A),
@@ -80,7 +79,7 @@ Hazard_Detection Hazard_Detection(
 	.ID_Rs1(ID_Rs1),
 	.ID_Rs2(ID_Rs2),
 	.EX_MemRead(EX_MemRead),
-	.EX_Rd(EX_RD),
+	.EX_Rd(EX_Rd),
 	.NoOp(NoOp),
 	.PCWrite(PCWrite),
 	.Stall_o(Stall)
@@ -88,16 +87,16 @@ Hazard_Detection Hazard_Detection(
 
 MUX32 MUX_PC(
 	.src0_i(pc_next),
-	.src1_i(ID_branch_PC),
-	.select_i(ID_to_branch),
-	.res_o(IF_PC_i)
+	.src1_i(ID_branch_pc),
+	.select_i(ID_branch_ctr),
+	.res_o(IF_pc)
 );
 
 PC PC(
 	.clk_i(clk_i),
 	.rst_i(rst_i),
 	.PCWrite_i(PCWrite),
-	.pc_i(IF_PC_i),
+	.pc_i(IF_pc),
 	.pc_o(pc)
 );
 
@@ -109,16 +108,16 @@ Adder Add_PC(
 
 Instruction_Memory Instruction_Memory(
 	.addr_i(pc),
-	.instr_o(IF_IR)
+	.instr_o(IF_instr)
 );
 
 Pipeline_IF_ID Pipeline_IF_ID(
 	.clk_i(clk_i),
 	.rst_i(rst_i),
-	.instr_i(IF_IR),
+	.instr_i(IF_instr),
 	.pc_i(pc),
-	.instr_o(ID_IR),
-	.pc_o(ID_PC),
+	.instr_o(ID_instr),
+	.pc_o(ID_pc),
 	.Flush_i(ID_FlushIF),
 	.Stall_i(Stall)
 );
@@ -139,17 +138,17 @@ Registers Registers(
 );
 
 Imm_Gen Imm_Gen(
-	.instr(ID_IR),
-	.immed(imme_o)
+	.instr(ID_instr),
+	.immed(immed)
 );
 
 Sign_Extend Sign_Extend(
-	.data_i(imme_o),
-	.data_o(ID_imme)
+	.data_i(immed),
+	.data_o(ID_immed)
 );
 
 Control Control(
-	.Op_i(ID_IR[6:0]),
+	.Op_i(ID_instr[6:0]),
 	.NoOp_i(NoOp),
 	.ALUOp_o(ID_ALUOp),
 	.ALUSrc_o(ID_ALUSrc),
@@ -165,71 +164,71 @@ Pipeline_ID_EX Pipeline_ID_EX(
 	.rst_i(rst_i),
 	.RS1data_i(ID_data1),
 	.RS2data_i(ID_data2),
-	.immed_i(ID_imme),
-	.pc_i(ID_PC),
-	.Rd_i(ID_RD),
+	.immed_i(ID_immed),
+	.pc_i(ID_pc),
+	.Rd_i(ID_Rd),
 	.RegWrite_i(ID_RegWrite),
 	.MemtoReg_i(ID_MemtoReg),
 	.MemRead_i(ID_MemRead),
 	.MemWrite_i(ID_MemWrite),
 	.ALUOp_i(ID_ALUOp),
 	.ALUSrc_i(ID_ALUSrc),
-	.instr_i(ID_IR),
+	.instr_i(ID_instr),
 	.RS1addr_i(ID_Rs1),
 	.RS2addr_i(ID_Rs2),
-	.RS1data_o(EX_A),
-	.RS2data_o(EX_B),
-	.immed_o(EX_imme),
-	.pc_o(EX_PC),
-	.Rd_o(EX_RD),
+	.RS1data_o(EX_data1),
+	.RS2data_o(EX_data2),
+	.immed_o(EX_immed),
+	.pc_o(EX_pc),
+	.Rd_o(EX_Rd),
 	.RegWrite_o(EX_RegWrite),
 	.MemtoReg_o(EX_MemtoReg),
 	.MemRead_o(EX_MemRead),
 	.MemWrite_o(EX_MemWrite),
 	.ALUOp_o(EX_ALUOp),
 	.ALUSrc_o(EX_ALUSrc),
-	.instr_o(EX_IR),
+	.instr_o(EX_instr),
 	.RS1addr_o(EX_Rs1),
 	.RS2addr_o(EX_Rs2)
 );
 
 MUX32_Double MUX_First(
-	.src00_i(EX_A),
+	.src00_i(EX_data1),
 	.src01_i(WB_RDdata),
 	.src10_i(MEM_ALUout),
 	.src11_i(0),
 	.select_i(Forward_A),
-	.res_o(MUX_A_o)
+	.res_o(MUX_First_o)
 );
 
 MUX32_Double MUX_Second(
-	.src00_i(EX_B),
+	.src00_i(EX_data2),
 	.src01_i(WB_RDdata),
 	.src10_i(MEM_ALUout),
 	.src11_i(0),
 	.select_i(Forward_B),
-	.res_o(MUX_B_o)
+	.res_o(MUX_Second_o)
 );
 
 MUX32 MUX_ALUSrc(
-	.src0_i(MUX_B_o),
-	.src1_i(EX_imme),
+	.src0_i(MUX_Second_o),
+	.src1_i(EX_immed),
 	.select_i(EX_ALUSrc),
-	.res_o(ALU_B_i)
+	.res_o(ALUdata2_i)
 );
 
 
 ALU ALU(
-	.src1_i(ALU_A_i),
-	.src2_i(ALU_B_i),
+	.src1_i(MUX_First_o),
+	.src2_i(ALUdata2_i),
 	.ALUCtr_i(ALUctl),
 	.res_o(EX_ALUout)
 );
 
 ALU_Control ALU_Control(
 	.ALUOp_i(EX_ALUOp),
-	.funct7_i(EX_IR[31:25]),
-	.funct3_i(EX_IR[14:12]),
+	.funct7_i(EX_instr[31:25]),
+	.funct3_i(EX_instr[14:12]),
 	.ALUCtr_o(ALUctl)
 );
 
@@ -237,15 +236,15 @@ Pipeline_EX_MEM Pipeline_EX_MEM(
 	.clk_i(clk_i),
 	.rst_i(rst_i),
 	.ALUout_i(EX_ALUout),
-	.WriteData_i(MUX_B_o),
-	.Rd_i(EX_RD),
+	.WriteData_i(MUX_Second_o),
+	.Rd_i(EX_Rd),
 	.RegWrite_i(EX_RegWrite),
 	.MemtoReg_i(EX_MemtoReg),
 	.MemRead_i(EX_MemRead),
 	.MemWrite_i(EX_MemWrite),
 	.ALUout_o(MEM_ALUout),
-	.WriteData_o(MEM_WD),
-	.Rd_o(MEM_RD),
+	.WriteData_o(MEM_WriteData),
+	.Rd_o(MEM_Rd),
 	.RegWrite_o(MEM_RegWrite),
 	.MemtoReg_o(MEM_MemtoReg),
 	.MemRead_o(MEM_MemRead),
@@ -259,20 +258,20 @@ Data_Memory Data_Memory(
 	.addr_i(MEM_ALUout),
 	.MemRead_i(MEM_MemRead),
 	.MemWrite_i(MEM_MemWrite),
-	.data_i(MEM_WD),
+	.data_i(MEM_WriteData),
 
-	.data_o(MEM_MD)
+	.data_o(MEM_MemoryData)
 );
 
 Pipeline_MEM_WB Pipeline_MEM_WB(
 	.clk_i(clk_i),
 	.rst_i(rst_i),
-	.MemoryData_i(MEM_MD),
+	.MemoryData_i(MEM_MemoryData),
 	.ALUout_i(MEM_ALUout),
-	.Rd_i(MEM_RD),
+	.Rd_i(MEM_Rd),
 	.RegWrite_i(MEM_RegWrite),
 	.MemtoReg_i(MEM_MemtoReg),
-	.MemoryData_o(WB_MD),
+	.MemoryData_o(WB_MemoryData),
 	.ALUout_o(WB_ALUout),
 	.Rd_o(WB_Rd),
 	.RegWrite_o(WB_RegWrite),
@@ -281,7 +280,7 @@ Pipeline_MEM_WB Pipeline_MEM_WB(
 
 MUX32 MUX_WB(
 	.src0_i(WB_ALUout),
-	.src1_i(WB_MD),
+	.src1_i(WB_MemoryData),
 	.select_i(WB_MemtoReg),
 	.res_o(WB_RDdata)
 );
